8d74dd39fed132122245133c30418ce2f0a90644
rafal-gorecki <126687345+rafal-gorecki@users.noreply.github.com>
2023-11-09 15:33:29 +0100

    Update healthcheck.cpp

--- healthcheck.cpp
+++ healthcheck.cpp
@@ -4,15 +4,14 @@
 
 using namespace std::chrono_literals;
 
-#define TOPIC_NAME "/camera/color/image_raw"
 #define TIMEOUT 2s
 
-int msg_received = EXIT_FAILURE;
+bool msg_received = false;
 
 void msg_callback(const sensor_msgs::msg::Image::SharedPtr msg)
 {
   std::cout << "Message received" << std::endl;
-  msg_received = EXIT_SUCCESS;
+  msg_received = true;
   rclcpp::shutdown();
 }
 
@@ -26,9 +25,9 @@ int main(int argc, char* argv[])
 {
   rclcpp::init(argc, argv);
   auto node = rclcpp::Node::make_shared("healthcheck_node");
-  auto sub = node->create_subscription<sensor_msgs::msg::Image>(TOPIC_NAME, rclcpp::SensorDataQoS(), msg_callback);
+  auto sub = node->create_subscription<sensor_msgs::msg::Image>("/camera/color/image_raw", rclcpp::SensorDataQoS(), msg_callback);
   auto timer = node->create_wall_timer(TIMEOUT, timeout_callback);
 
   rclcpp::spin(node);
-  return msg_received;
+  return msg_received ? EXIT_SUCCESS : EXIT_FAILURE;
 }
