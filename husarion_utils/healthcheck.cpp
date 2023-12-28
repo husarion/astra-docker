d03b505803e8cd0581ffc74287301cf52f3cba08
rafal-gorecki <rafal.gorecki@husarion.com>
2023-12-28 15:24:34 +0100

    Merge with `namespace`

--- husarion_utils/healthcheck.cpp
+++ husarion_utils/healthcheck.cpp
@@ -4,45 +4,55 @@
 
 using namespace std::chrono_literals;
 
-#define LOOP_PERIOD 500ms
+#define HEALTHCHECK_PERIOD 500ms
 #define MSG_VALID_TIME 2s
 
-std::chrono::steady_clock::time_point last_msg_time;
+class HealthCheckNode : public rclcpp::Node {
+public:
+  HealthCheckNode()
+      : Node("healthcheck_astra"),
+        last_msg_time(std::chrono::steady_clock::now()) {
 
-void write_health_status(const std::string &status) {
-  std::ofstream healthFile("/var/tmp/health_status.txt");
-  healthFile << status;
-}
+    subscription_ = create_subscription<sensor_msgs::msg::Image>(
+        "color/image_raw", rclcpp::SensorDataQoS().keep_last(1),
+        std::bind(&HealthCheckNode::msgCallback, this, std::placeholders::_1));
 
-void msg_callback(const sensor_msgs::msg::Image::SharedPtr msg) {
-  last_msg_time = std::chrono::steady_clock::now();
-}
-
-void healthy_check() {
-  std::chrono::steady_clock::time_point current_time =
-      std::chrono::steady_clock::now();
-  std::chrono::duration<double> elapsed_time = current_time - last_msg_time;
-  bool is_msg_valid = elapsed_time < MSG_VALID_TIME;
-
-  if (is_msg_valid) {
-    write_health_status("healthy");
-  } else {
-    write_health_status("unhealthy");
+    timer_ = create_wall_timer(HEALTHCHECK_PERIOD,
+                               std::bind(&HealthCheckNode::healthyCheck, this));
   }
-}
+
+private:
+  std::chrono::steady_clock::time_point last_msg_time;
+  rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr subscription_;
+  rclcpp::TimerBase::SharedPtr timer_;
+
+  void writeHealthStatus(const std::string &status) {
+    std::ofstream healthFile("/var/tmp/health_status.txt");
+    healthFile << status;
+  }
+
+  void msgCallback(const sensor_msgs::msg::Image::SharedPtr msg) {
+    RCLCPP_DEBUG(get_logger(), "Msg arrived");
+    last_msg_time = std::chrono::steady_clock::now();
+  }
+
+  void healthyCheck() {
+    std::chrono::steady_clock::time_point current_time =
+        std::chrono::steady_clock::now();
+    std::chrono::duration<double> elapsed_time = current_time - last_msg_time;
+    bool is_msg_valid = elapsed_time < MSG_VALID_TIME;
+
+    if (is_msg_valid) {
+      writeHealthStatus("healthy");
+    } else {
+      writeHealthStatus("unhealthy");
+    }
+  }
+};
 
 int main(int argc, char *argv[]) {
   rclcpp::init(argc, argv);
-  auto node = rclcpp::Node::make_shared("healthcheck_astra");
-  auto sub = node->create_subscription<sensor_msgs::msg::Image>(
-      "camera/color/image_raw", rclcpp::SensorDataQoS().keep_last(1),
-      msg_callback);
-
-  while (rclcpp::ok()) {
-    rclcpp::spin_some(node);
-    healthy_check();
-    std::this_thread::sleep_for(LOOP_PERIOD);
-  }
-
+  rclcpp::spin(std::make_shared<HealthCheckNode>());
+  rclcpp::shutdown();
   return 0;
 }
