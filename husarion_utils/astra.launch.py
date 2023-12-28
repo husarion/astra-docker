6c9425eb1c586d44b3b4a8a332d93059660be8d4
rafal-gorecki <126687345+rafal-gorecki@users.noreply.github.com>
2023-12-28 15:35:07 +0100

    Image optimization (#11)
    * Add pre-commit (#8)
    
    * iron no supported
    
    * Change health_status dir
    
    * Build Release & Helthcheck decrease interval
    
    * Add pre-commit
    
    * Optimized image
    
    * Merge with `namespace`
    
    * Backward compatibility

--- husarion_utils/astra.launch.py
+++ husarion_utils/astra.launch.py
@@ -1,46 +1,44 @@
 from launch import LaunchDescription
-from launch_ros.actions import Node, PushRosNamespace
 from launch.actions import DeclareLaunchArgument, OpaqueFunction
+from launch_ros.actions import Node, PushRosNamespace
 from launch.substitutions import EnvironmentVariable, LaunchConfiguration
-
-
-def replace_rviz_fixed_frame(input_file, output_file, fix_frame):
-    with open(input_file, "r") as file:
-        rviz_content = file.read()
-
-    rviz_content = rviz_content.replace("Fixed Frame: camera_link", f"Fixed Frame: {fix_frame}")
-
-    with open(output_file, "w") as file:
-        file.write(rviz_content)
 
 
 def launch_setup(context, *args, **kwargs):
     robot_namespace = LaunchConfiguration("robot_namespace").perform(context)
     sensor_namespace = LaunchConfiguration("sensor_namespace").perform(context)
 
-    default_rviz_conf = "/root/.rviz2/default.rviz"
-    rviz_conf = "/root/.rviz2/modified_default.rviz"
-    replace_rviz_fixed_frame(default_rviz_conf, rviz_conf, f"{sensor_namespace}_link")
-
     remapping = []
     if robot_namespace:
-        remapping.append(("/clicked_point", f"/{robot_namespace}/clicked_point"))
-        remapping.append(("/goal_pose", f"/{robot_namespace}/goal_pose"))
-        remapping.append(("/initialpose", f"/{robot_namespace}/initialpose"))
         remapping.append(("/tf", f"/{robot_namespace}/tf"))
         remapping.append(("/tf_static", f"/{robot_namespace}/tf_static"))
 
-    rviz = Node(
-        package="rviz2",
-        executable="rviz2",
-        name="rviz2",
-        namespace=sensor_namespace,
+    params_file = "/husarion_utils/astra_params.yaml"
+
+    astra_node = Node(
+        package="astra_camera",
+        executable="astra_camera_node",
+        name="camera",
+        namespace="camera",
+        parameters=[
+            params_file,
+            {
+                "camera_name": sensor_namespace,
+            },
+        ],
         remappings=remapping,
-        arguments=["-d", rviz_conf],
         output="screen",
     )
 
-    return [PushRosNamespace(robot_namespace), rviz]
+    healthcheck_node = Node(
+        package="healthcheck_pkg",
+        executable="healthcheck_node",
+        name="healthcheck_astra",
+        namespace=sensor_namespace,
+        output="screen",
+    )
+
+    return [PushRosNamespace(robot_namespace), astra_node, healthcheck_node]
 
 
 def generate_launch_description():
@@ -57,6 +55,5 @@ def generate_launch_description():
                 description="Sensor namespace that will appear after all topics and TF frames, used for distinguishing multiple cameras on the same robot.",
             ),
             OpaqueFunction(function=launch_setup),
-            PushRosNamespace(LaunchConfiguration("robot_namespace")),
         ]
     )
